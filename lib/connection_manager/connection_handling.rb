5963c68ae6ec3550c306346e2cf1e9af83e0058b
Joshua Mckinney <joshmckin@gmail.com>
2014-06-08 15:31:52 -0500

    never mind

--- lib/connection_manager/connection_handling.rb
+++ lib/connection_manager/connection_handling.rb
@@ -26,13 +26,6 @@ module ConnectionManager
     end
     alias :database_name= :schema_name=
 
-    def use_database(database_name=nil,opts={})
-      warn "[DEPRECATION] `use_database` is deprecated.  Please use `schema_name=` instead."
-      self.table_name = opts[:table_name] if opts[:table_name]
-      self.table_name_prefix = opts[:table_name_prefix] unless opts[:table_name_prefix].blank?
-      self.schema_name=database_name
-    end
-
     # A place to store managed connections
     def managed_connections
       @@managed_connections
