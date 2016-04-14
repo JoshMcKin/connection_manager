6ff48814dfd5e053f09880879bc193d56c1e5cc9
Joshua T. Mckinney <joshmckin@gmail.com>
2016-04-14 10:44:15 -0500

    refactor replication.rb

--- lib/connection_manager/replication.rb
+++ lib/connection_manager/replication.rb
@@ -1,20 +1,17 @@
 module ConnectionManager
   module Replication
-    attr_accessor :replication_connections
+
     # Replication methods (replication_method_name, which is the option[:name] for the
     # #replication method) and all their associated connections. The key is the
     # replication_method_name and the value is an array of all the replication_classes
     # the replication_method has access to.
     #
     # EX: replication_methods[:slaves] => ['Slave1Connection',Slave2Connection]
-    def replication_connections
-      @replication_connections ||= {:slaves => [], :masters => []}
-    end
-
+    attr_accessor :replication_connections
 
     # Is this class replicated
     def replicated?
-      (@replication_connections && (!replication_connections[:slaves].empty? || !replication_connections[:masters].empty?))
+      defined?(@replication_connections)
     end
 
     # Builds a class method that returns an ActiveRecord::Relation for use with
@@ -30,7 +27,11 @@ module ConnectionManager
     # * :name - name of class method to call to access replication, default to slaves
     # * :type - the type of replication; :slaves or :masters, defaults to :slaves
     def replicated(*connections)
-      options = {:slaves => [], :masters => [], :type => :slaves}.merge(connections.extract_options!.symbolize_keys)
+      options = connections.extract_options!
+      options.symbolize_keys!
+      options[:slaves] ||= []
+      options[:masters] ||= []
+      options[:type] ||= :slaves
       options[options[:type]] = connections unless connections.empty?
       set_replications_connections(options)
     end
@@ -50,9 +51,9 @@ module ConnectionManager
     # connections we use sample to get a random connection instead of blocking
     # to rotate the pool on every fetch.
     def fetch_replication_connection(method_name)
-      set_replications_connections(self.replication_connections) unless self.replicated?
-      available_connections = self.replication_connections[method_name] || []
-      raise ArgumentError, "No connections found for #{method_name}." if available_connections.blank?
+      unless @replication_connections && available_connections = @replication_connections[method_name]
+        raise ArgumentError, "Replication connections could not be found for #{method_name}."
+      end
       available_connections.sample
     end
 
@@ -61,14 +62,15 @@ module ConnectionManager
       [:masters,:slaves].each do |type|
         cons = (options[type].empty? ? connection.replications[type] : options[type])
         unless cons.empty?
-          self.replication_connections[type] = []
+          @replication_connections ||= {}
           cons.each do |to_use|
-            self.replication_connections[type] << fetch_connection_class_name(to_use)
+            @replication_connections[type] ||= []
+            @replication_connections[type] << fetch_connection_class_name(to_use)
           end
         end
       end
-      raise ArgumentError, "Connections could not be found for #{self.name}." if self.replication_connections[:masters].empty? && self.replication_connections[:slaves].empty?
-      self.replication_connections
+      raise ArgumentError, "Replication connections could not be found for #{self.name}." if !replicated? || (@replication_connections[:masters].nil? && @replication_connections[:slaves].nil?)
+      @replication_connections
     end
 
     def fetch_connection_class_name(to_use)
